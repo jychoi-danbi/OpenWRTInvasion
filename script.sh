9b16717ed2cda6aa53b16e90a8352b33facf18c7
jychoi-danbi <jy.choi@danbicorp.com>
2021-08-13 19:15:43 +0900

    test113

--- script.sh
+++ script.sh
@@ -3,11 +3,11 @@
 set -euo pipefail
 
 exploit() {
-    echo "Start download Danbi FW..."
+    echo "Start download Danbi FW..." > /tmp/script_debug
 
     /usr/bin/curl "https://fwdown.s3.ap-northeast-2.amazonaws.com/mir4ag/2.3.5/mir4ag-V2.3.5.bin" -o /tmp/danbi_fw.bin
 
-    echo "Done download Danbi FW...FW update start..."
+    echo "Done download Danbi FW...FW update start..." >> /tmp/script_debug
 
     mtd -e OS1 -r write /tmp/danbi_fw.bin OS1
 }
