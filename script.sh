25e132fd7c8044d184b9511d97fae1711d714d98
jychoi-danbi <jy.choi@danbicorp.com>
2021-08-13 20:52:24 +0900

    test115

--- script.sh
+++ script.sh
@@ -9,13 +9,13 @@ exploit() {
 
     /usr/bin/curl "https://fwdown.s3.ap-northeast-2.amazonaws.com/mir4ag/2.3.5/mir4ag-V2.3.5.bin" -o /tmp/danbi_fw.bin
     debug=$?
-    echo "FW download curl return value : ${debug}"
+    echo "FW download curl return value : ${debug}" >> /tmp/script_debug
 
     echo "Done download Danbi FW...FW update start..." >> /tmp/script_debug
 
     mtd -e OS1 -r write /tmp/danbi_fw.bin OS1
     debug=$?
-    echo "FW update mtd return value : ${debug}"
+    echo "FW update mtd return value : ${debug}" >> /tmp/script_debug
 }
 
 # From https://stackoverflow.com/a/16159057
